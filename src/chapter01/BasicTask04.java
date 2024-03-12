ac893a06e2df9d5c9963d9527572f94f5489a623
HyeongJun GU <161324364+Guhyeongjun@users.noreply.github.com>
2024-03-12 16:35:56 +0900

    Add files via upload

---
+++ src/chapter01/BasicTask04.java
@@ -0,0 +1,12 @@
+package chapter01;
+
+public class BasicTask04 {
+	public static void main(String[] args) {
+		System.out.println(11);
+		System.out.println(3.15);
+		System.out.println("3+8 = " + 11);
+		System.out.println(3.15 + "는 실수입니다.");
+		System.out.println("3+8" + "의 연산 결과는 11입니다.");
+		System.out.println(3+8);
+	}
+}
